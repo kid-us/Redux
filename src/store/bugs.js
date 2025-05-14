6d06dadcaf4ef30752bac5544ddbf1b8185ecd02
kid-us <kidushh29@gmail.com>
2025-05-14 13:05:15 +0300

    Create Reducers

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,31 +1,35 @@
-import { createAction } from "@reduxjs/toolkit";
-
-// Creating Actions
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugResolved");
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-  // Bug Added
-  if (action.type === bugAdded.type)
-    return [
-      ...state,
-      {
+// Actions
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved"); // ✅ fix action type
+
+// Modern reducer using builder callback
+const reducer = createReducer([], (builder) => {
+  builder
+    // Create Bug
+    .addCase(bugAdded.type, (bugs, action) => {
+      bugs.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
-      },
-    ];
-  // Bug Removed
-  else if (action.type === bugRemoved.type)
-    return state.filter((bug) => bug.id !== action.payload.id);
-  // Bug Resolved
-  else if (action.type === bugResolved.type)
-    return state.map((bug) =>
-      bug.id !== action.payload.id ? bug : { ...bug, resolved: true }
-    );
+      });
+    })
 
-  return state;
-}
+    // Update Resolved
+    .addCase(bugResolved.type, (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      if (index !== -1) bugs[index].resolved = true;
+    })
+
+    // Remove Bug
+    .addCase(bugRemoved.type, (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      if (index !== -1) bugs.splice(index, 1);
+    });
+});
+
+export default reducer;
