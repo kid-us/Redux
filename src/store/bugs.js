d685582f5ce5e9aab499d66d87d010f4ca95b354
kid-us <kidushh29@gmail.com>
2025-05-14 13:07:57 +0300

    Create Slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,35 +1,34 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastId = 0;
 
-// Actions
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved"); // ✅ fix action type
-
-// Modern reducer using builder callback
-const reducer = createReducer([], (builder) => {
-  builder
-    // Create Bug
-    .addCase(bugAdded.type, (bugs, action) => {
+const bugSlice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // Add Bugs
+    bugAdded: (bugs, action) => {
       bugs.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
       });
-    })
+    },
 
     // Update Resolved
-    .addCase(bugResolved.type, (bugs, action) => {
+    bugResolved: (bugs, action) => {
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       if (index !== -1) bugs[index].resolved = true;
-    })
+    },
 
-    // Remove Bug
-    .addCase(bugRemoved.type, (bugs, action) => {
+    // Delete Bug
+    bugRemoved: (bugs, action) => {
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       if (index !== -1) bugs.splice(index, 1);
-    });
+    },
+  },
 });
 
-export default reducer;
+export const { bugAdded, bugRemoved, bugResolved } = bugSlice.actions;
+
+export default bugSlice.reducer;
