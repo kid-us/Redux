13291502ae82c69f9ac4e9c7a1a4c1575b4d7508
kid-us <kidushh29@gmail.com>
2025-05-15 15:37:26 +0300

    api call

--- src/index.js
+++ src/index.js
@@ -1,54 +1,65 @@
-import {
-  bugAdded,
-  bugRemoved,
-  bugResolved,
-  bugAddedToUser,
-  getUnresolvedBugs,
-  getBugsByUser,
-} from "./store/bugs";
+// import {
+//   bugAdded,
+//   bugRemoved,
+//   bugResolved,
+//   bugAddedToUser,
+//   getUnresolvedBugs,
+//   getBugsByUser,
+// } from "./store/bugs";
 
-import {
-  addProject,
-  removeProject,
-  updatePurchase,
-  getPurchasedProjects,
-} from "./store/projects";
+// import {
+//   addProject,
+//   removeProject,
+//   updatePurchase,
+//   getPurchasedProjects,
+// } from "./store/projects";
+
+// import { addUsers } from "./store/users";
 
 import createStore from "./store/store";
-import { addUsers } from "./store/users";
+
+import * as actions from "./store/api";
 
 const store = createStore();
 
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store Changed", store.getState());
-// });
+store.dispatch(
+  actions.apiCallBegan({
+    url: "/bugs",
+    onSuccess: "bugsReceived",
+    // onError: actions.apiCallFailed.type,
+  })
+);
 
-// Bugs
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(bugAdded({ description: "Bug 4" }));
-// store.dispatch(bugResolved({ id: 1 }));
-// store.dispatch(bugRemoved({ id: 4 }));
-// store.dispatch(bugAddedToUser({ bugId: 1, userId: 1 }));
+// // const unsubscribe = store.subscribe(() => {
+// //   console.log("Store Changed", store.getState());
+// // });
 
-// Projects
+// // Bugs
+// // store.dispatch(bugAdded({ description: "Bug 1" }));
+// // store.dispatch(bugAdded({ description: "Bug 2" }));
+// // store.dispatch(bugAdded({ description: "Bug 3" }));
+// // store.dispatch(bugAdded({ description: "Bug 4" }));
+// // store.dispatch(bugResolved({ id: 1 }));
+// // store.dispatch(bugRemoved({ id: 4 }));
+// // store.dispatch(bugAddedToUser({ bugId: 1, userId: 1 }));
+
+// // Projects
 // store.dispatch(addProject({ name: "JavaScript" }));
-// store.dispatch(addProject({ name: "TypeScript" }));
-// store.dispatch(addProject({ name: "ReactNative" }));
-// store.dispatch(addProject({ name: "React" }));
-// store.dispatch(removeProject({ id: 4 }));
-// store.dispatch(updatePurchase({ id: 2 }));
+// // store.dispatch(addProject({ name: "TypeScript" }));
+// // store.dispatch(addProject({ name: "ReactNative" }));
+// // store.dispatch(addProject({ name: "React" }));
+// // store.dispatch(removeProject({ id: 4 }));
+// // store.dispatch(updatePurchase({ id: 2 }));
 
-// Users
-store.dispatch(addUsers({ name: "Lorem" }));
+// // Users
+// store.dispatch(addUsers({ name: "Lorem" }));
 
-// unsubscribe();
+// // unsubscribe();
 
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-const notPurchasedProjects = getPurchasedProjects(store.getState());
-const bugsByUser = getBugsByUser(1)(store.getState());
+// const unresolvedBugs = getUnresolvedBugs(store.getState());
+// const notPurchasedProjects = getPurchasedProjects(store.getState());
+// const bugsByUser = getBugsByUser(1)(store.getState());
 
-console.log(unresolvedBugs);
-console.log(notPurchasedProjects);
-console.log(bugsByUser);
+// console.log(unresolvedBugs);
+// console.log(notPurchasedProjects);
+// console.log(bugsByUser);
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducers";
-import logger from "./middleware/logger";
+// import logger from "./middleware/logger";
+import api from "./middleware/api";
 
 export default function createStore() {
   return configureStore({
     reducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(logger({ destination: "console" })),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api),
   });
 }
